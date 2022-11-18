eec6fb2615af228a6f44ab082c1c9c44e5585311
welliston <welliston_fabio@hotmail.com>
2022-11-18 18:39:17 -0300

    Segunda Video aula

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import.meta.glob([
+    '../images/**',
+]);
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ resources/views/teste.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Aprendendo juntos</title>
+</head>
+<body>
+    <h1 class="bg-cyan-700">Olá Mundo!</h1>
+</body>
+</html>
